a39c779106f2c788b7b76c1088182524acab0075
RiskaMahardika <riskamahardika77@gmail.com>
2024-01-16 09:04:33 -0800

    cara menghitung anak tangga()
    nama:I kadek riska mahardika
    nim:2313231036
    kita membuat code menghitung anak tangga code dibagi menjadi 4
    
    ini code ke 2

--- anak_tangga.cpp
+++ anak_tangga.cpp
@@ -27,7 +27,6 @@ int hitungCaraMencapaiPuncak(int n, set<int>& langkahDiperbolehkan) {
 
     return cara;
 }
-
 void jml_cara(int n, set<int>& langkahDiperbolehkan, string jalurSaatIni) {
     if (n < 0) return;
     if (n == 0) {
