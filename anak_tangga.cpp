618d659d496889a76b289b3ce3ab94e777a104c7
Aril-linganata <155123923+Aril-linganata@users.noreply.github.com>
2024-01-16 21:42:23 +0800

    cara mengghitung anak_tangga()
    nama:  I Made Aril Linganata
    nim   : 2313231020
    
    kita membuat code menghitung anak_tangga code dibagi menjadi 4
    ini code pertama (1)

--- anak_tangga.cpp
+++ anak_tangga.cpp
@@ -2,14 +2,29 @@
 #include <map>
 #include <set>
 
+using namespace std;
 
-void jml_cara(int n){
+map<int, int> steps;
 
-//CODE
+int hitungCaraMencapaiPuncak(int n, set<int>& langkahDiperbolehkan) {
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+
+    // Periksa apakah hasil untuk langkah saat ini sudah dimemoisasi
+    if (steps.find(n) != steps.end()) {
+        return steps[n];
+    }
+
+    int cara = 0;
+
+    // Hitung jumlah cara dengan mempertimbangkan setiap langkah yang diperbolehkan
+    for (int langkah : langkahDiperbolehkan) {
+        cara += hitungCaraMencapaiPuncak(n - langkah, langkahDiperbolehkan);
+    }
+
+    // Memoisasi hasil untuk langkah saat ini
+   steps[n] = cara;
+
+    return cara;
 }
 
-int main(){
-map <int, int> steps;
-
-return 0;
-}
